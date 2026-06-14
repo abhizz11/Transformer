b0f68f9680eb8f457b10004769889932109706fe
abhizz11 <abhinavpneupane@gmail.com>
2026-06-14 00:02:38 -0500

    Added Simplified Attention class that can also handle weights

--- attentionweights.py
+++ attentionweights.py
@@ -1,6 +1,6 @@
 # Implementing attention mechanism with trainable weights (Q,K,V) matrices
 import torch 
-
+import torch.nn as nn
 # Attention mechanism for "Your journey starts here"
 
 inputs = torch.tensor(
@@ -48,3 +48,28 @@ context_vec_2 = attn_weights_2 @ values
 print(context_vec_2)
 
 # Now, we are creating a class to make it compact
+
+class SelfAttention(nn.Module):
+
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1] ** 0.5, dim=-1
+        )
+
+        context_vec = attn_weights @ values 
+
+        return context_vec
+
+sa = SelfAttention(d_in, d_out)
+print(sa(inputs))
