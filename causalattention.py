d5db979d6840ec83905c7353d74e1b1877887346
abhizz11 <abhinavpneupane@gmail.com>
2026-06-14 23:04:30 -0500

    Added forward function in the class and ran a simple example with past inputs

--- causalattention.py
+++ causalattention.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import attentionweights
 
-'''Practice part
+
 inputs = torch.tensor(
   [[0.43, 0.15, 0.89], # Your     (x^1)
    [0.55, 0.87, 0.66], # journey  (x^2)
@@ -11,7 +11,7 @@ inputs = torch.tensor(
    [0.77, 0.25, 0.10], # one      (x^5)
    [0.05, 0.80, 0.55], # step     (x^6)
 ])
-
+'''Practice part
 torch.manual_seed(789)
 
 x_2 = inputs[1] # A
@@ -50,17 +50,42 @@ dropout = torch.nn.Dropout(0.1) # We turn 10 % of the neurons randomly to make t
 
 print(dropout(attn_weights))
 '''
-
+batch = torch.stack((inputs, inputs), dim=0)
 class CausalAttention(nn.Module):
     
     def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
         super().__init__()
         self.d_out = d_out
-        self.W_query = nn.Linear(d_in, d_out, qkv_bias=qkv_bias)
-        self.W_key = nn.Linear(d_in, d_out, qkv_bias=qkv_bias)
-        self.W_value = nn.Linear(d_in, d_out, qkv_bias=qkv_bias)
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.dropout = nn.Dropout(dropout)
         self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
     
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape # batch dimension number of tokens, and their dimension
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
 
-        
+        attn_scores = queries @ keys.transpose(1,2) # We just want to transpose each of the batch
+        attn_scores.masked_fill_(
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf
+        ) # using num_tokens to handle edge cases where num tokens < context length
+
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim = -1
+        )
+        attn_weights = self.dropout(attn_weights) # Implementing dropout
+
+        context_vec = attn_weights @ values 
+        return context_vec 
+  
+d_in = inputs.shape[1]
+d_out = 2
+torch.manual_seed(123)
+context_length = batch.shape[1]
+ca = CausalAttention(d_in, d_out, context_length, 0.0)
+context_vecs = ca(batch)
+print(ca)
+print(context_vecs.shape)
