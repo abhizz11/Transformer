b53218107bac977e67ac009c58aaff1a252fb61d
abhizz11 <abhinavpneupane@gmail.com>
2026-06-21 19:32:42 -0500

    Implemeted MultiHeadedAttention Class extending on CausalAttention Class, removed the wrapper class

--- multiheadedattention.py
+++ multiheadedattention.py
@@ -1,32 +1,82 @@
-import causalattention
 import torch 
 import torch.nn as nn
 
-# Multiheaded attention is causal attention stacked on top of each other
-class MultiHeadAttentionWrapper(nn.Module):
 
-    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias= False):
         super().__init__()
-        self.heads = nn.ModuleList(
-            [causalattention.CausalAttention(d_in, d_out, context_length, dropout, qkv_bias) for _ in range(num_heads)]
-        )
-    
-    def forward(self, x):
-        return torch.cat([head(x) for head in self.heads], dim=-1)
+        assert(d_out % num_heads == 0), \
+        "d_out must be divisible by num_heads"
 
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias) # Query matrix
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias) # Key matrix
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias) # Value matrix
+        self.out_proj = nn.Linear(d_out, d_out) # To combine output heads later
+        self.dropout = nn.Dropout(dropout) # To randomy turn off some of the neurons
+
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        ) # To mask future tokens
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        # We split the matrix by adding "num_heads" dimension, converting d_out => nheads, headdim
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Transpose, so attention is calculated independently for each head
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        # Compute attn scores
+        attn_scores = queries @ keys.transpose(2, 3) # Dot product for each head
+
+        # To mask future tokens 
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf) # Masking with -infinity
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # Retranspose to get Context vectors
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        # Combine
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec) 
+
+        return context_vec
+
+
+
+torch.manual_seed(123)
+
+# Define the tensor with 3 rows and 6 columns
 inputs = torch.tensor(
-  [[0.43, 0.15, 0.89], # Your     (x^1)
-   [0.55, 0.87, 0.66], # journey  (x^2)
-   [0.57, 0.85, 0.64], # starts   (x^3)
-   [0.22, 0.58, 0.33], # with     (x^4)
-   [0.77, 0.25, 0.10], # one      (x^5)
-   [0.05, 0.80, 0.55], # step     (x^6)
-])
+    [[0.43, 0.15, 0.89, 0.55, 0.87, 0.66],  # Row 1
+     [0.57, 0.85, 0.64, 0.22, 0.58, 0.33],  # Row 2
+     [0.77, 0.25, 0.10, 0.05, 0.80, 0.55]]  # Row 3
+)
 
 batch = torch.stack((inputs, inputs), dim=0)
-context_length = batch.shape[1]
-d_in, d_out = 3, 2
-mha = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads=2)
+print(batch.shape) 
+
+batch_size, context_length, d_in = batch.shape
+d_out = 6
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
 context_vecs = mha(batch)
 print(context_vecs)
-print(context_vecs.shape)
+print("context_vecs.shape:", context_vecs.shape)
