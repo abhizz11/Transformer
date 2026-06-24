4fd7c69b720a48e1854f81ee12d84fd25a8a7f76
abhizz11 <abhinavpneupane@gmail.com>
2026-06-23 19:23:06 -0500

    Added the code for DummyNormalizationClass. Renamed it to be Normalization class

--- dummy_gpt.py
+++ dummy_gpt.py
@@ -25,7 +25,7 @@ class DummyGPTModel(nn.Module):
         )
         
         # Just a placeholder for LayerNorm
-        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.final_norm = LayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear(
             cfg["emb_dim"], cfg["vocab_size"], bias=False
         )
@@ -38,6 +38,7 @@ class DummyGPTModel(nn.Module):
         x = self.drop_emb(x)
         x = self.trf_blocks(x)
         x = self.final_norm(x)
+
         logits = self.out_head(x)
         return logits 
 
@@ -50,13 +51,23 @@ class DummyTransformerBlock(nn.Module):
     def forward(self, x):
         return x 
 
-# Just a dummer layer for normalization
-class DummyLayerNorm(nn.Module):
-    def __init__(self, normalized_shape, eps=1e-5):
+# Normalization class
+class LayerNorm(nn.Module):
+    '''
+    The goal of this class is to force input to have a mean of 0 and a variance of 1.
+    Forcing the layers to do that, prevents the vanishing gradient problem.
+    '''
+    def __init__(self, emb_dim):
         super().__init__()
-    
+        self.eps = 1e-5 # To ensure we don't have division by zero errors
+        self.scale = nn.Parameter(torch.ones(emb_dim)) # scale, shift give us the flexibility to "undo" the normalization 
+        self.shift = nn.Parameter(torch.zeros(emb_dim)) # Helps us reshape the distribution
+
     def forward(self, x):
-        return x 
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False) # unbiased = False so that we divide by n instead of n - 1
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift         
 
 tokenizer = tiktoken.get_encoding("gpt2")
 batch = []
@@ -72,4 +83,3 @@ torch.manual_seed(123)
 model = DummyGPTModel(GPT_CONFIG_124M)
 logits = model(batch)
 print(logits.shape)
-print(logits)
